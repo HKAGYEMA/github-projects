9e09786d2c29480f76437a6e07acaef3445b4f96
Harris Kofi Agyeman <32367385+HKAGYEMA@users.noreply.github.com>
2019-05-08 21:39:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 import requests
 from parsel import Selector
+import scrapy
 #import openpyxl as op
 #import pandas as pd
 from scrapy.spiders import CrawlSpider, Rule
@@ -32,15 +33,55 @@ for link in href_links:
                 end = time.time()
                 print("Time taken in seconds : ", (end-start))
 
-class PagesSpider(CrawlSpider):
-    name = "pages"
-    allowed_domains = ['http://example.webscraping.com']
-    base_path = ['http://example.webscraping.com']
+#class PagesSpider(CrawlSpider):
+    #name = "pages"
+    #allowed_domains = ['http://example.webscraping.com']
+    #base_path = ['http://example.webscraping.com']
 
-    rules = (
-        Rule(LinkExtractor(allow=(), restrict_css=('.pageNextPrev',)),
-             callback="parse_item",
-             follow=True),)
+    #rules = (
+        #Rule(LinkExtractor(allow=(), restrict_css=('.pageNextPrev',)),
+             #callback="parse_item",
+             #follow=True),)
 
-    def parse_item(self, response):
-        print('Processing..' + response.url)
+#def parse_item(self, response):
+    #all_links = response.css(' .large > .detailslisk::attr(href)') .extract()
+    #for a in all_links:
+        #yield scrapy.Request(a, callback=self.parse)
+    #print('Processing..' + response.url)
+
+#def parse(self, response):
+
+    #title = response.css('title::text') .extract_first()
+            #get anchor tags
+    #links = response.css('a::attr(href)') .extract()
+
+  
+  
+class ExtractUrls(scrapy.Spider): 
+    name = "extract"
+  
+    # request function 
+def start_requests(self): 
+        urls = [ 'http://example.webscraping.com', ] 
+          
+        for url in urls: 
+            yield scrapy.Request(url = url, callback = self.parse) 
+  
+    # Parse function 
+def parse(self, response): 
+          
+        # Extra feature to get title 
+        title = response.css('title::text').extract_first()  
+          
+        # Get anchor tags 
+        links = response.css('a::attr(href)').extract()      
+          
+        for link in links: 
+            yield 
+            { 
+                'title': title, 
+                'links': link 
+            } 
+              
+            if 'webcrawler' in link:          
+                yield scrapy.Request(url = link, callback = self.parse)
