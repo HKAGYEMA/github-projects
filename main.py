1cdf4403a94957ac53d38e2df05c7f42a7ebeb38
Harris Kofi Agyeman <32367385+HKAGYEMA@users.noreply.github.com>
2019-05-08 21:10:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
+
 import requests
 from parsel import Selector
-import openpyxl as op
-import pandas as pd
-
+#import openpyxl as op
+#import pandas as pd
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
 import time
 start = time.time()
 
 
-
 all_links = []
-response = requests.get('http://example.webscraping.com')
+base_path = 'http://example.webscraping.com'
+response = requests.get(base_path)
 selector = Selector(response.text)
 href_links = selector.xpath('//a/@href').getall()
 all_links += href_links
@@ -17,38 +19,28 @@ all_links += href_links
 sheet_name = "URLs"
 
 for link in href_links:
-	    try:
-	        response = requests.get(link)
-	        selector = Selector(response.text)
-	        href_links = selector.xpath('//a/@href').getall()
-	        all_links += href_links
-	        if len(all_links) >=1000:
-	          break
-	    except Exception as exp:
-	        print('Error navigating to link : ', link)
-print(all_links)
-end = time.time()
-print("Time taken in seconds : ", (end-start))
+    try:
+        response = requests.get(base_path + link)
+        selector = Selector(response.text)
+        href_links_2 = selector.xpath('//a/@href').getall()
+        all_links += href_links_2
+        if len(all_links) >=10:
+            break
+    except Exception as exp:
+                print('Error navigating to link : ', link)
+                print(all_links)
+                end = time.time()
+                print("Time taken in seconds : ", (end-start))
 
-soup = BeautifulSoup("")
-urls = soup.findAll("a")
+class PagesSpider(CrawlSpider):
+    name = "pages"
+    allowed_domains = ['http://example.webscraping.com']
+    base_path = ['http://example.webscraping.com']
 
-    # Even if the url is not part of the same domain, it is still collected
-    # But those urls not in the same domain are not parsed
-    for a in urls:
-        if (a.get("href")) and (a.get("href") not in url_list):
-            url_list.append(a.get("href"))
+    rules = (
+        Rule(LinkExtractor(allow=(), restrict_css=('.pageNextPrev',)),
+             callback="parse_item",
+             follow=True),)
 
-def load_to_excel(lst):
-    """
-    Load the list into excel file using pandas
-    """
-    # Load list to dataframe
-    df = pd.DataFrame(lst)
-    df.index += 1  # So that the excel column starts from 1
-
-    # Write dataframe to excel
-    xlw = pd.ExcelWriter(xl_name)
-    df.to_excel(xlw, sheet_name=sheet_name, index_label="#", header=["URL"])
-    xlw.save()
-
+    def parse_item(self, response):
+        print('Processing..' + response.url)
