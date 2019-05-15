ef33c69d0fe7465809f7b1acc8f92f8b1526e6a7
Harris Kofi Agyeman <32367385+HKAGYEMA@users.noreply.github.com>
2019-05-15 11:52:59 +0100

    Update cw3.py

--- cw3.py
+++ cw3.py
@@ -1,7 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
 from time import sleep
-
+import numpy
+global index, iMap
+index = []
+iMap = []
 def getNext(result):
     #result = requests.get(url)
     soup = BeautifulSoup(result, 'html.parser')
@@ -29,12 +32,44 @@ def country_data(string):
         i = 1+i
     # Remove empty things and sort them
     sortSite = site.split(" ")
-    for i in range(1, len(sortSite)-1):
-        if sortSite[i] == '':
-            sortSite.pop(i)
+    while '' not in sortSite:#for i in range(0, len(sortSite)-1):
+        sortSite.remove('')
+            #sortSite.pop(i)
     sortSite.sort()
     return sortSite
 
+def invert(link,result):
+    global index
+    global iMap
+    x= 0
+    if index == []:
+        index = result
+        array = numpy.ones(len(result), numpy.int8)
+        iMap.append([link, array.tolist()])
+    else:
+        tmp = []
+        for word in index:
+            if word not in result:
+                tmp.append(0)
+            else:
+                count = 0
+                try:
+                    while result.remove(word) != ValueError:
+                        count+=1
+                except:
+                    pass#print('')
+                finally:
+                    tmp.append(count)
+        # Now add the rest to the list
+        for y in range(len(result)):
+            if y > 1 and result[y]==result[y-1]:
+                tmp[-1] += 1
+            elif y > 1 and result[y]!=result[y-1]:
+                tmp.append(1)
+                index.append(result[y])        
+        iMap.append([link,tmp])
+    x+=1
+    #print(index, iMap)
 
 def run():
     nav = "" # used to navigate the website
@@ -48,10 +83,29 @@ def run():
         for result in results:
             sleep(1)
             country = result.findChildren("a")
-            print(country_data(link+country[0]['href']))
+            data = country_data(link+country[0]['href'])
+            invert(link+country[0]['href'], data)
+            print('* Success *',country[0]['href'])
         nav = getNext(page.text)
         #print(page.text)
         if nav == None:
             break
+    return iMap,index
 
-run()
+maps,index = run()
+for row in maps:
+    for i in range(len(row[1]),len(index)):
+        row[1].append(0)
+inp = 'Dollar'
+if inp not in index:
+    print(index)
+    print('not in the index')
+else:
+    i = index.index(inp)
+    #going through all the rows look for matching word
+    for j in range(len(maps)):
+        #print(index,'\n',maps[j][1], i)
+        if maps[j][1][i] > 0:
+            print('Site:\t',maps[j][0],'\nTimes:\t', maps[j][1][i])
+
+#print (maps)
